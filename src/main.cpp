1f15e773b3830c2f52d3c3495ff3c89e3cda4bc7
James Patrick Keegan <atomicbomber.email@gmail.com>
2018-01-15 15:39:49 +0700

    Turned on vsync, added fixed time step feature.

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,31 @@
 
 #include <SFML/Graphics.hpp>
 
+
+const int MAX_FPS = 60;
+const int MICROSECS_PER_FRAME = 1000000 / MAX_FPS;
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(640, 480), "Program", sf::Style::Titlebar);
+    window.setVerticalSyncEnabled(true);
 
+    sf::Clock clock;
+    u_int elapsed = 0;
     while (window.isOpen()) {
+
+        elapsed += clock.getElapsedTime().asMicroseconds();
+        clock.restart();
 
         window.clear(sf::Color::White);
         window.display();
+
+        // Update loop
+        while (elapsed < MICROSECS_PER_FRAME) {
+            // Do somehting cool
+
+            elapsed -= MICROSECS_PER_FRAME;
+        }
 
         sf::Event event;
         while (window.pollEvent(event)) {
